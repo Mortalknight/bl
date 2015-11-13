a3de6a7ec3cc0d7f71b378df56162d100f100a63
Jake <jake.jerner@gmail.com>
2015-11-13 12:55:12 +0100

    libs

---
+++ includes.php
@@ -0,0 +1,21 @@
+<?php
+//include libs
+require_once('lib/medoo.php');
+
+
+//configs
+
+$database = new medoo([
+	// required
+	'database_type' => 'mysql',
+	'database_name' => 'bl',
+	'server' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8',
+ 
+	// [optional]
+	'port' => 3306
+
+]);
+?>
